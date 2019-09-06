6a9c5d4d47761780b385a127d5586689f22c2871
panage-jp <joojoojoo6217@gmail.com>
2019-09-06 12:19:33 +0900

    omniauth settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   has_many :sns_credentials
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # 登録済みuserはそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
