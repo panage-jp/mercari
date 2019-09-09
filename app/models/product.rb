dba6042063cc305543fd6aea1151812c7de133a7
panage-jp <joojoojoo6217@gmail.com>
2019-09-09 17:23:01 +0900

    add asocciations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :product_categories
+  has_many :product_brands
+  has_many :categories, through: :product_categories
+  has_many :brands, through: :product_brands
+  belongs_to :user
 end
